a360d74d1f06f85804c54ecde11afa1337c4dddc
abc8806191 <49439251+abc8806191@users.noreply.github.com>
2023-02-15 02:46:40 +0800

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,24 @@
+sudo echo root:yuan5201314 |sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo pip3 install rsa
+sudo pip3 install thrift==0.11.0
+sudo pip3 install requests
+sudo pip3 install pytz
+sudo pip3 install bs4
+sudo pip3 install googletrans
+sudo pip3 install youtube_dl
+sudo pip3 install pafy
+sudo pip3 install humanfriendly
+sudo pip3 install asyncio
+sudo pip3 install humanize
+sudo pip3 install hyper
+sudo apt install speedtest-cli
+sudo pip3 install twder
+sudo pip3 install httplib2
+sudo pip3 install python-axolotl-curve25519
+sudo pip3 install requests_futures
+sudo pip3 install pycryptodome
+sudo ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
+sudo apt upgrade -y
+sudo reboot
