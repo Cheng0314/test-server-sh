5d2d112ac870b45e5d9edad6aa97dae65afa2875
Cheng <70269751+Cheng0314@users.noreply.github.com>
2024-07-08 16:33:36 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,24 +4,23 @@ sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/s
 sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
+sudo apt install speedtest-cli -y
 sudo pip3 install rsa
-sudo pip3 install thrift==0.11.0
+sudo pip3 install thrift
 sudo pip3 install requests
 sudo pip3 install pytz
 sudo pip3 install bs4
-sudo pip3 install googletrans
-sudo pip3 install youtube_dl
 sudo pip3 install pafy
 sudo pip3 install humanfriendly
 sudo pip3 install asyncio
 sudo pip3 install humanize
 sudo pip3 install hyper
-sudo apt install speedtest-cli -y
 sudo pip3 install twder
 sudo pip3 install httplib2
 sudo pip3 install python-axolotl-curve25519
 sudo pip3 install requests_futures
+sudo pip3 install pycrypto
 sudo pip3 install pycryptodome
+sudo pip3 install aiofile
 sudo ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
-sudo apt upgrade -y
 sudo reboot
